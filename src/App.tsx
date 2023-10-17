84b50a50d0e8b48d9a8d90f9e80a031844d34b66
Claudio Maesi <claudiomaesi@gmail.com>
2023-10-17 17:06:45 -0300

    finalizando projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
+import { useState } from 'react'
+import { ThemeProvider } from 'styled-components'
 import Projetos from './containers/Projetos'
 import SideBar from './containers/SideBar'
 import Sobre from './containers/SideBar/Sobre'
 import EstiloGlobal, { Container } from './styles'
 
+import temaDark from './themes/dark'
+import temaLight from './themes/light'
+
 function App() {
+  const [estaUsandoTemadark, setEstaUsandoTemaDark] = useState(false)
+
+  function trocaTema() {
+    setEstaUsandoTemaDark(!estaUsandoTemadark)
+  }
+
   return (
-    <>
+    <ThemeProvider theme={estaUsandoTemadark ? temaDark : temaLight}>
       <EstiloGlobal />
       <Container>
-        <SideBar />
+        <SideBar trocaTema={trocaTema} />
         <main>
           <Sobre />
           <Projetos />
         </main>
       </Container>
-    </>
+    </ThemeProvider>
   )
 }
 
