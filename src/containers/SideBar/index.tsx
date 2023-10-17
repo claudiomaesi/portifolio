a4281cb5a2f3058bea1a98f80a824f8283557885
Claudio Maesi <claudiomaesi@gmail.com>
2023-10-17 16:10:05 -0300

    estilizando sidebar

--- src/containers/SideBar/index.tsx
+++ src/containers/SideBar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SideBarContainer } from './styles'
 
 const SideBar = () => (
   <aside>
-    <img src="https://github.com/claudiomaesi.png" />
-    <Titulo fontSize={20}>Cláudio Maesi</Titulo>
+    <SideBarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Cláudio Maesi</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        claudiomaesi
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Dev Full Stack Python
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SideBarContainer>
   </aside>
 )
 
